018b36c4edf040cca567910f7fb491fb222d69d8
Simon Bielk <simonbielk@hotmail.com>
2015-06-06 15:03:03 +0200

    Fixed rtf error

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1347\cocoasubrtf570
-\cocoascreenfonts1{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue173;\red13\green103\blue1;\red210\green0\blue5;
-}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl480
-
-\f0\fs28 \cf0 \expnd0\expndtw0\kerning0
-<html>\
-\'a0<head>\
-\'a0\'a0<title>PHP\'a0Test</title>\
-\'a0</head>\
-\'a0<body>\
-\'a0\cf2 \expnd0\expndtw0\kerning0
-<?php\'a0\cf3 \expnd0\expndtw0\kerning0
-echo\'a0\cf4 \expnd0\expndtw0\kerning0
-'<p>Hello\'a0World</p>'\cf3 \expnd0\expndtw0\kerning0
-;\'a0\cf2 \expnd0\expndtw0\kerning0
-?>\cf0 \expnd0\expndtw0\kerning0
-\'a0\
-\'a0</body>\
-</html>}
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
